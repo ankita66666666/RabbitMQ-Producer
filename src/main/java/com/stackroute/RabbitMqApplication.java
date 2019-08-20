eabe01a7cd1f13fab9ed22f46d1ea9a1544eef06
Ankita <kumariankita.gaurav@cgi.com>
2019-08-20 10:03:29 +0530

    Dockerized RabbitMQ producer

--- src/main/java/com/stackroute/RabbitMqApplication.java
+++ src/main/java/com/stackroute/RabbitMqApplication.java
@@ -3,9 +3,7 @@ package com.stackroute;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Enables Spring Boot auto config and component scanning.
- */
+
 @SpringBootApplication
 public class RabbitMqApplication {
 
--- src/main/java/com/stackroute/controller/ProducerController.java
+++ src/main/java/com/stackroute/controller/ProducerController.java
@@ -6,28 +6,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * RestController annotation is used to create Restful web services using Spring MVC
- */
 @RestController
 
-/**
- * RequestMapping annotation maps HTTP requests to handler methods
- */
 @RequestMapping(value = "api/v1")
 public class ProducerController {
-
     @Autowired
     private ProducerService producerService;
-
-    /**
-     * Constructor based Dependency injection to inject TrackService into controller
-     */
 
     public ProducerController(ProducerService producerService) {
         this.producerService = producerService;
     }
-
 
     @RequestMapping("/send")
     public String sendMessage(@RequestParam("msg") String msg) {
