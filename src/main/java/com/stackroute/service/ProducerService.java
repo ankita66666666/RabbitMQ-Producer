777eb10cbb05d1226fddedf2a1005f3bef972625
ankita66666666 <52036280+ankita66666666@users.noreply.github.com>
2019-08-20 16:56:03 +0530

    Update ProducerService.java

--- src/main/java/com/stackroute/service/ProducerService.java
+++ src/main/java/com/stackroute/service/ProducerService.java
@@ -5,22 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-/**
- * @Service indicates annotated class is a service which hold business logic in the Service layer
- */
 @Service
 public class ProducerService {
 
 
-    /**
-     * Constructor based Dependency injection to inject AmqpTemplate here
-     */
     @Autowired
     private AmqpTemplate amqpTemplate;
 
-    /**
-     * @Value injects values into fields
-     */
     @Value("${jsa.rabbitmq.exchange}")
     private String exchange;
 
